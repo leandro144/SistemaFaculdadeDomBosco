842d5b5f765f8193cab4aeb5c559e83a883054eb
LeandroCastro <leandro_castro980@hotmail.com>
2023-11-10 17:40:55 -0300

    add postgreeSQL

--- src/pages/validator/[id].js
+++ src/pages/validator/[id].js
@@ -14,7 +14,7 @@ function ApiData() {
 
 
   useEffect(() => {
-    fetch(`https://sistema-faculdade-dom-bosco.vercel.app/validator/${id}`) // Substitua pela URL da sua API com o ID
+    fetch(`/validator/${id}`) // Substitua pela URL da sua API com o ID
       .then((response) => response.json())
       .then((data) => setData(data))
       .catch((error) => console.error("Erro ao buscar dados:", error));
