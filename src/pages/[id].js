093b6efed6f948817ec1b038f353209847cf01d7
Leandro Castro <leandro_castro980@hotmail.com>
2023-11-12 23:02:08 -0300

    db

--- src/pages/[id].js
+++ src/pages/[id].js
@@ -13,7 +13,7 @@ function ApiData() {
 
 
   useEffect(() => {
-    fetch(`http://localhost:8080/validator/${id}`)
+    fetch(`https://sistema-faculdade-dom-bosco.vercel.app//validator/${id}`)
     .then((response) => {
     if (!response.ok) {
       throw new Error(`Erro na requisição: ${response.status}`);
