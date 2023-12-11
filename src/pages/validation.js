0aec3e37f20cc86659ea831122dd24ec6d8ed2b2
Leandro Castro <leandro_castro980@hotmail.com>
2023-12-11 09:45:01 -0300

    add new studant

--- src/pages/validation.js
+++ src/pages/validation.js
@@ -3,67 +3,62 @@ import { useRouter } from 'next/router';
 import Header from '@/components/Header';
 import Menu from '@/components/Menu';
 import { Flex, FormControl, FormLabel, Input, Button, Text, useDisclosure } from '@chakra-ui/react';
+import api from '../api/services'
 import ApiModal from '@/components/ApiModal';
 
 const Validation = () => {
-  const [inputData, setInputData] = useState('');
-  const [user, setUser] = useState('');
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const router = useRouter();
+    const [inputData, setInputData] = useState('');
+    const [user, setUser] = useState('');
+    const { isOpen, onOpen, onClose } = useDisclosure();
 
-  const handleSearch = async () => {
-    if (inputData === '') {
-      alert("Por favor, preencha este campo");
-      return;
-    }
-  
-    try {
-      const response = await fetch(`http://localhost:8080/dados/${inputData}`);
-      if (!response.ok) {
-        throw new Error(`Erro ao buscar: ${response.statusText}`);
-      }
-  
-      const data = await response.json();
-      setUser(data);
-      onOpen();
-    } catch (error) {
-      console.error("Erro ao buscar:", error);
-      alert("Erro ao buscar");
-    }
-  };
-  
+    const handleSearch = async () => {
+        if (inputData === '') {
+            alert("Por favor preencha este campo");
+            return;
+        }
 
-  return (
-    <>
-      <Header />
-      <Menu />
-      <Flex
-        justifyContent={'center'}
-        flexDir={'column'}
-        alignItems={'center'}
-        w={'100%'}
-        h={'90vh'}
-        bg={'#053057'}
-      >
-        <FormControl as={'fieldset'} maxW={'428px'} color={'#fff'} py={4}>
-          <FormLabel as={'legend'}>
-            <Text fontWeight={'700'} fontSize="1rem" color={'#fff'} textAlign='center'>Validador de Diploma Digital e Histórico Escolar</Text>
-          </FormLabel>
-          <Flex gap={4}>
-            <Input
-              type='text'
-              placeholder='Digite o código'
-              value={inputData}
-              onChange={(e) => setInputData(e.target.value)}
-            />
-            <Button onClick={handleSearch}>Enviar</Button>
-          </Flex>
-        </FormControl>
+        try {
+            const response = await api.get(`${inputData}`);
+            setUser(response.data);
+            onOpen();
+            setInputData('');
+        } catch {
+            alert("Erro ao buscar");
+            setInputData('');
+        }
+    };
 
-        <ApiModal isOpen={isOpen} onClose={onClose} apiData={user} />
-      </Flex>
-    </>
-  );
+    return (
+        <>
+            <Header />
+            <Menu />
+            <Flex
+                justifyContent={'center'}
+                flexDir={'column'}
+                alignItems={'center'}
+                w={'100%'}
+                h={'90vh'}
+                bg={'#053057'}
+            >
+                <FormControl as={'fieldset'} maxW={'428px'} color={'#fff'} py={4}>
+                    <FormLabel as={'legend'}>
+                        <Text fontWeight={'700'} fontSize="1rem" color={'#fff'} textAlign='center'>Validador de Diploma Digital e Historico Escolar</Text>
+                    </FormLabel>
+                    <Flex gap={4}>
+                        <Input
+                            type='text'
+                            placeholder='Digite o código'
+                            value={inputData}
+                            onChange={(e) => setInputData(e.target.value)}
+                        />
+                        <Button onClick={handleSearch}>Enviar</Button>
+                    </Flex>
+                </FormControl>
+
+                <ApiModal isOpen={isOpen} onClose={onClose} apiData={user} />
+            </Flex>
+        </>
+    );
 }
 
 export default Validation;
